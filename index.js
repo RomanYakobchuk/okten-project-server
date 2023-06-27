da98bd42bf63f010a69d49da05bdd637a41da551
Roman Yakobchuk <71780751+RomanYakobchuk@users.noreply.github.com>
2023-06-27 21:22:03 +0300

    Update index.js

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ app.use(function (req, res, next) {
 const io = new Server(server,
     {
         cors: {
-            origin: ['http://localhost:3000'],
+            origin: configs.CLIENT_URL,
         }
     }
 )
